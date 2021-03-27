dc97b6a8ec489ba794250dff1063318bc4fce5f9
warley.xavier <warley.xavier@eagletecnologia.com.br>
2021-03-26 22:38:08 -0300

    implementao status bar padão da aplicação

---
+++ src/app/components/StatusBar/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  descricao: {
+    fontFamily: "Roboto",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black"
+  }
+});
---
+++ src/app/pages/Categoria/ConsultaCategorias/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import StatusBar from "../../../components/StatusBar";
+
+import { styles } from "./styles";
+
+export default function ConsultaCategorias() {
+  return(
+    <View style={styles.container}>
+      <StatusBar/>
+    </View>
+  )
+}
